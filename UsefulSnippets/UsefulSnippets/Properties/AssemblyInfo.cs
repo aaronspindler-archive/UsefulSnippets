82cf8511761e88d4e24e70b3b986b9bae3eaa14e
xNovax <aaron@xnovax.net>
2015-08-30 14:11:34 -0400

    Modified assembly information

--- UsefulSnippets/UsefulSnippets/Properties/AssemblyInfo.cs
+++ UsefulSnippets/UsefulSnippets/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UsefulSnippets")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A set of utilities that are used frequently")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("xNovax")]
 [assembly: AssemblyProduct("UsefulSnippets")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
