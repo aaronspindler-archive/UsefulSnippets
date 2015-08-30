975908f608381e05732608b27f59c9af709bda76
xNovax <aaron@xnovax.net>
2015-08-30 14:09:13 -0400

    Added Classes and Tests

---
+++ UsefulSnippets/UsefulSnippets.Tests/NetworkUnitTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace UsefulSnippets.Test
+{
+    [TestClass]
+    public class NetworkUnitTests
+    {
+        [TestMethod]
+        public void CheckForInternetConnectionTestMethod()
+        {
+            Boolean actualData = Network.CheckForInternetConnection();
+            const bool expectedData = true;
+
+            Assert.AreEqual(expectedData, actualData, "Internet connectivity check is not as expected");
+        }
+    }
+}
